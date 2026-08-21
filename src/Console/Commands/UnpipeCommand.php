f9671f54670e9575918c98fbbd4b0e1b561c143e
Samgu Lee <cable8mm@gmail.com>
2026-08-22 01:23:20 +0900

    fix: run `composer lint`

--- src/Console/Commands/UnpipeCommand.php
+++ src/Console/Commands/UnpipeCommand.php
@@ -6,11 +6,11 @@ namespace Cable8mm\PromptWeaver\Console\Commands;
 
 use RuntimeException;
 use Symfony\Component\Console\Helper\QuestionHelper;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 final class UnpipeCommand extends PromptWeaverCommand
 {
