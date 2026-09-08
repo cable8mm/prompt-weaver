6eb4ba05e47f308b63b0390990789154ecfd2397
Samgu Lee <cable8mm@gmail.com>
2026-09-08 18:20:52 +0900

    fix: update font sizes

--- src/Enums/Format.php
+++ src/Enums/Format.php
@@ -52,12 +52,12 @@ enum Format: string
             self::A45_POSTER => [
                 'box_width_pc' => 70,
                 'box_height_pc' => 8,
-                'font_size_pt' => 18,
+                'font_size_pt' => 24,
             ],
             self::A67_POSTER, self::MINI_SQUARE => [
                 'box_width_pc' => 80,
                 'box_height_pc' => 12,
-                'font_size_pt' => 10,
+                'font_size_pt' => 18,
             ],
         };
     }
