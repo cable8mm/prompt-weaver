8eaf8b4357d5a64d9cf87299fbd39b371d8e375f
Samgu Lee <cable8mm@gmail.com>
2026-09-09 21:13:19 +0900

    feat: remove 'split' layout support and update related documentation and tests

--- src/Enums/Layout.php
+++ src/Enums/Layout.php
@@ -10,7 +10,6 @@ enum Layout: string
 
     case CENTERED = 'centered';
     case EDITORIAL = 'editorial';
-    case SPLIT = 'split';
     case QR_FOCUS = 'qr-focus';
     case MINI_SQUARE = 'mini-square';
 
@@ -19,7 +18,6 @@ enum Layout: string
         return match ($this) {
             self::CENTERED => 'Centered',
             self::EDITORIAL => 'Editorial',
-            self::SPLIT => 'Split composition',
             self::QR_FOCUS => 'QR focus',
             self::MINI_SQUARE => 'Mini Square composition',
         };
